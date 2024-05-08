d5185a3a676ce0498505fda94254a7dbb497c9bd
danielemalitesta <d.malitesta@gmail.com>
2024-05-08 13:19:21 +0200

    Correct config files for UltraGCN.

--- cmd_custom.py
+++ cmd_custom.py
@@ -75,8 +75,8 @@ def main():
 #SBATCH --time=24:00:00                                   ## format: HH:MM:SS
 #SBATCH --nodes=1
 #SBATCH --mem=20GB                                       ## memory per node out of 494000MB (481GB)
-#SBATCH --output=$HOME/slogs/SisInf_Topology_output-%A_%a.out
-#SBATCH --error=$HOME/slogs/SisInf_Topology_error-%A_%a.err
+#SBATCH --output=$HOME/graph_topology/slogs/SisInf_Topology_output-%A_%a.out
+#SBATCH --error=$HOME/graph_topology/slogs/SisInf_Topology_error-%A_%a.err
 #SBATCH --account={1}
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user={2}
